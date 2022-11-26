d0f031df4d5cba64d308b010538b3bf9c4866d83
İsmail <112792309+Ismkol@users.noreply.github.com>
2022-11-26 23:37:10 +0300

    Add files via upload

---
+++ homework3.sql
@@ -0,0 +1,18 @@
+--SELECT country FROM country
+--WHERE country 
+--LIKE 'A%a'
+
+--SELECT country FROM country
+--WHERE country 
+--LIKE '%_____n'
+--ORDER BY country ASC;
+
+--SELECT title FROM film
+--WHERE title  ILIKE 'T%T%T%T%'
+--OR title ILIKE '%t%t%t%t'
+
+--SELECT * FROM film
+--WHERE title
+--LIKE 'C%'
+--AND length > 90
+--AND rental_rate = 2.99;
